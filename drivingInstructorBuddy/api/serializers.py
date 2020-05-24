2e0e4cb0ad032eb6543962b6c3b8ccaa0fbda2df
Yash Khatri <yashkhatri@yashs-macbook-pro.local>
2020-05-24 18:15:24 +0100

    Add user registration, verification and token genration on login feature.

--- drivingInstructorBuddy/api/serializers.py
+++ drivingInstructorBuddy/api/serializers.py
@@ -2,13 +2,25 @@ import random
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from drivingInstructorBuddy.models import Profile
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
 
 User = get_user_model()
 
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = "__all__"
+
+class UserSerializer(serializers.ModelSerializer):
+    profile = UserProfileSerializer()
+    class Meta:
+        model = User
+        exclude = ['password', 'verification_code', 'is_superuser', 'is_active']
 
 class UserCreateSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, style={
-        "input_type": "password"})
+        "input_type": "password"}, max_length=128)
     email = serializers.CharField(write_only=True, required=True, style={
         "input_type": "email"})
     name = serializers.CharField(max_length=128)
@@ -41,18 +53,29 @@ class UserCreateSerializer(serializers.ModelSerializer):
 class UserVerifySerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(required=True)
     verification_code = serializers.IntegerField(required=True)
-
     class Meta:
         model = User
-        fields = ["id", "verification_code"]
+        fields = [
+            "id",
+            "verification_code"
+        ]
 
     def update(self, instance, validated_data):
-
-        print(validated_data['verification_code'])
         print(validated_data)
         if instance.verification_code != validated_data['verification_code']:
             raise serializers.ValidationError(
                 {"verification": "Incorrect verification code!"})
         instance.is_verified = True
         instance.save()
+        return instance
+
+
+class CustomTokenTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data['is_verified'] = self.user.is_verified
+        data['id']=self.user.id
+        return data
+
+
 
--- drivingInstructorBuddy/urls.py
+++ drivingInstructorBuddy/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from drivingInstructorBuddy.api.viewsets import registration, verify_user
-
-from .viewsets import NoteViewSet
+from drivingInstructorBuddy.api.viewsets import registration, verify_user, CustomTokenObtainPairView
 
 router = SimpleRouter()
 # router.register('drivingInstructorBuddy', NoteViewSet, basename="notes")
@@ -11,7 +9,7 @@ router = SimpleRouter()
 urlpatterns = [
     path("register/", registration, name='register'),
     path("verify/", verify_user, name='verify_user'),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/", CustomTokenObtainPairView.as_view(), name="custom_token_obtain_pair"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
 
